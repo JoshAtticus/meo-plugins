11b5bb47d8eb20549b70373c480144ea09f60ac1
3r1s_s <127928354+3r1s-s@users.noreply.github.com>
2024-07-06 19:34:54 -0700

    Yeah

--- archbtw.js
+++ archbtw.js
@@ -1,7 +1,7 @@
 const actuallysendpost = sendpost
 sendpost = function() {
   const msgbox = document.getElementById('msg');
-  if (msgbox.disabled) return;
+  if (msgbox.disabled || msgbox.value.trim() === "") return;
   if (!msgbox.value.includes("i use arch btw")) {
     msgbox.value += " i use arch btw"
   };
--- sentfrom.js
+++ sentfrom.js
@@ -2,8 +2,8 @@
 const actuallysendpost = sendpost;
 sendpost = function() {
   const msgbox = document.getElementById('msg');
-  if (msgbox.disabled) return;
-  
+  if (msgbox.disabled || msgbox.value.trim() === "") return;
+  // for future plugins of this kind PLEASE ADD THIS
   const ua = navigator.userAgent;
   if (!msgbox.value.includes(ua)) {
     msgbox.value += ` 
