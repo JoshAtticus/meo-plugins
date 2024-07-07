b733c0b26c1a64115503804a361f4d5322f9b3f6
Arrow <89811520+ArrowAced@users.noreply.github.com>
2024-07-07 17:15:29 -0400

    fix formatting on nuhhuh.js

--- nuhhuh.js
+++ nuhhuh.js
@@ -1,8 +1,8 @@
 const actuallyerimd = erimd
 erimd = function(content) {
-  const text = content 
-		.replace("&lt;:yuhhuh:1227268820213698611&gt;", "__TEMPYUHHUH__") 
-		.replace("&lt;:nuhhuh:1233290735999258664&gt;", "&lt;:yuhhuh:1227268820213698611&gt;")
+    const text = content 
+        .replace("&lt;:yuhhuh:1227268820213698611&gt;", "__TEMPYUHHUH__") 
+        .replace("&lt;:nuhhuh:1233290735999258664&gt;", "&lt;:yuhhuh:1227268820213698611&gt;")
 		.replace("__TEMPYUHHUH__", "&lt;:nuhhuh:1233290735999258664&gt;")
 	return actuallyerimd(text)
 }
