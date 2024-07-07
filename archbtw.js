8d4774701d1f8f9995b2d410c17ba796488065a4
3r1s_s <127928354+3r1s-s@users.noreply.github.com>
2024-07-06 20:16:13 -0700

    smol update

--- archbtw.js
+++ archbtw.js
@@ -1,7 +1,8 @@
 const actuallysendpost = sendpost
 sendpost = function() {
   const msgbox = document.getElementById('msg');
-  if (msgbox.disabled || msgbox.value.trim() === "") return;
+  if (msgbox.disabled) return;
+  if (msgbox.value.trim() === "" && pendingAttachments.length === 0) return;
   if (!msgbox.value.includes("i use arch btw")) {
     msgbox.value += " i use arch btw"
   };
--- sentfrom.js
+++ sentfrom.js
@@ -2,7 +2,8 @@
 const actuallysendpost = sendpost;
 sendpost = function() {
   const msgbox = document.getElementById('msg');
-  if (msgbox.disabled || msgbox.value.trim() === "") return;
+  if (msgbox.disabled) return;
+  if (msgbox.value.trim() === "" && pendingAttachments.length === 0) return;
   // for future plugins of this kind PLEASE ADD THIS
   const ua = navigator.userAgent;
   if (!msgbox.value.includes(ua)) {
