643ed8298cfede63a0eb3ee041b82685a78a0657
Josh <72828296+JoshAtticus@users.noreply.github.com>
2024-07-08 08:56:07 +0800

    Update blockSignatures.js

--- blockSignatures.js
+++ blockSignatures.js
@@ -10,6 +10,42 @@
                 nextElement.remove();
             }
         });
+
+        const postContents = document.querySelectorAll('p.post-content > p');
+        postContents.forEach(post => {
+            let text = post.textContent.trim();
+            if (text.endsWith('i use arch btw')) {
+                text = text.replace(/i use arch btw$/, '').trim();
+            }
+            if (text.includes('***')) {
+                text = text.replace(/\*\*\*/, '').trim();
+            }
+            if (text.includes('######')) {
+                text = text.replace(/######/, '').trim();
+            }
+            post.textContent = text;
+        });
+
+        const replies = document.querySelectorAll('div.reply-outer p');
+        replies.forEach(reply => {
+            let text = reply.textContent.trim();
+            if (text.includes('Sent from')) {
+                const splitText = text.split('Sent from');
+                if (splitText.length > 1) {
+                    text = splitText[0].trim();
+                }
+            }
+            if (text.endsWith('i use arch btw')) {
+                text = text.replace(/i use arch btw$/, '').trim();
+            }
+            if (text.includes('***')) {
+                text = text.replace(/\*\*\*/, '').trim();
+            }
+            if (text.includes('######')) {
+                text = text.replace(/######/, '').trim();
+            }
+            reply.textContent = text;
+        });
     }
 
     const observer = new MutationObserver((mutations) => {
